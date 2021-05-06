ba2859839c9ff163cdeef994d9d725f7adf3c763
sabaturgay <turgaysaba@outlook.com>
2021-05-06 18:38:54 +0300

    configure deployment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import { storeAPI, utilAPI } from './utils'
+import { storeAPI } from './utils'
 import BaseRoute from './pages'
 import { Loader } from './components'
 
 const { StoreContext } = storeAPI
 class App extends Component {
   state = {
-    isLoading: true,
+    isLoading: false,
     store: storeAPI.INITIAL_STORE,
   }
-
-  async componentDidMount() {
-    utilAPI.initializeApp(this)
-  }
-
 
   render() {
     const { state: { isLoading, store } } = this
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,3 @@
-import firebase from 'firebase/app'
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 import 'filepond/dist/filepond.min.css'
@@ -9,13 +8,3 @@ import 'react-toastify/dist/ReactToastify.css'
 import { registerPlugin } from 'react-filepond'
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
-
-const config = {
-  apiKey: 'AIzaSyBguRh5XZZf0Gqip2MCTz5yVgs3O1pqgP4',
-  authDomain: 'examplereact-62509.firebaseapp.com',
-  databaseURL: 'https://examplereact-62509.firebaseio.com',
-  projectId: 'examplereact-62509',
-  storageBucket: '',
-  messagingSenderId: '578715305063',
-}
-firebase.initializeApp(config)
